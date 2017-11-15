f438b1dc09e814d4b4877e4ad72bf3a436c3f33e
Krzysztof Lojniewski <kontakt@pagepro.pl>
2017-11-15 09:53:24 +0100

    PL calculation

--- js/main.js
+++ js/main.js
@@ -26,7 +26,7 @@ const pairList = [
         bid: 0.00000538
       }
     ],
-    coins: 5012.5
+    coins: 5012.5// C1
   },
   {
     name: 'ETH',
@@ -55,6 +55,7 @@ const app = new Vue({
     percentLimit: 3
   },
   methods: {
+    // E2
     getSellQty: function (coins, exchangeName) {
       return coins - (coins * this.getTakerFee(exchangeName))
     },
@@ -64,17 +65,29 @@ const app = new Vue({
     getExchangeByName: function (exchangeName) {
       return exchanges.filter(exchange => exchange.name === exchangeName)[0]
     },
+    // F2
     getSellCost: function (pair, exchangeName) {
       return pair.coins * this.getCourseByExchangeName(pair, exchangeName).ask
     },
     getCourseByExchangeName: function (pair, exchangeName) {
       return pair.courses.filter(exchange => exchange.name === exchangeName)[0]
     },
+    // G2
     getExchangesSpread: function (pair, buyExchangeName, sellExchangeName) {
       const buyExchange = this.getCourseByExchangeName(pair, buyExchangeName)
       const sellExchange = this.getCourseByExchangeName(pair, sellExchangeName)
       const spread = (sellExchange.bid * 100 / buyExchange.ask) - 100
       return Math.round(spread * 100) / 100
+    },
+    // PLValue - profit/loss value
+    getPLValue: function (pair, buyExchangeName, sellExchangeName) {
+      const coinsToSell = this.getSellQty(pair.coins, buyExchangeName)
+      const sellExchange = this.getCourseByExchangeName(pair, sellExchangeName)
+      const sellValue = coinsToSell * sellExchange.bid
+
+      const sellFee = sellValue * this.getTakerFee(sellExchangeName)
+
+      return sellValue - sellFee - this.getSellCost(pair, buyExchangeName)
     }
   }
 })
